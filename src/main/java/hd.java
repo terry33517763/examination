f43cea2fd0cc8b7bb8d21faca3d72687f53f5ea7
terry <>
2020-10-22 10:23:57 +0800

    Initial commit

---
+++ src/main/java/hd.java
@@ -0,0 +1,27 @@
+import java.util.logging.XMLFormatter;
+
+public class hd<A> {
+    public static void main(String[] args){
+    int A=268;
+    int X=0;
+    int X1;
+    int Y;
+    int Y1;
+    int Z;
+    int Z1;
+    int W;
+    int answer;
+    X=A/50;
+    X1=A%50;
+    Y=X1/10;
+    Y1=X1%10;
+    Z=Y1/5;
+    Z1=Y1%5;
+    W=Z1/1;
+    answer=X+Y+Z+W;
+    System.out.println(answer);
+ }
+}
+
+
+
